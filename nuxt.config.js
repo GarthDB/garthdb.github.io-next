4851f4e8d082d12de80a4e697c323835b1381879
garthdb <garthdb@gmail.com>
2020-05-25 16:52:11 -0600

    wip

---
+++ nuxt.config.js
@@ -0,0 +1,62 @@
+module.exports = {
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: 'garthdb.github.io-next',
+    meta: [{
+        charset: 'utf-8'
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1'
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Personal site for Garth Braithwaite'
+      }
+    ],
+    link: [{
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Alegreya:ital@0;1&family=Roboto+Slab:wght@300;400;700&display=swap'
+      }
+    ]
+  },
+  /*
+   ** Customize the progress bar color
+   */
+  loading: {
+    color: '#3B8070'
+  },
+  /*
+   ** Build configuration
+   */
+  css: [
+    'normalize.css/normalize.css',
+    '~/css/main.scss'
+  ],
+  build: {
+    /*
+     ** Run ESLint on save
+     */
+    extend(config, {
+      isDev,
+      isClient
+    }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,190 @@
+<template>
+  <article class="content">
+    <header class="headerMain">
+      <h1>Garth Braithwaite</h1>
+    </header>
+    <section class="section section--profile">
+      <h2 class="section__title">Profile</h2><hr>
+      <p class="section__description">Designer, developer, speaker, trainer, writer, open source advocate.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet porttitor eget dolor morbi non arcu. Vel pretium lectus quam id leo in vitae turpis. Nisi quis eleifend quam adipiscing vitae proin sagittis. Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque purus. Commodo ullamcorper a lacus vestibulum. Tempor id eu nisl nunc mi ipsum faucibus vitae. Malesuada pellentesque elit eget gravida. Pulvinar pellentesque habitant morbi tristique senectus et netus et malesuada. Congue quisque egestas diam in.</p>
+    </section>
+    <section class="section section--workHistory">
+      <h2 class="section__title">Work History</h2><hr>
+      <section class="institution company">
+        <h3 class="institution__name">Adobe</h3>
+        <h4 class="institution__jobTitle">Sr Computer Scientist Lead, <span class="instituion__date">2010</span></h4>
+        <p class="institution__jobDescription">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Interdum velit euismod in pellentesque.</p>
+        <h4 class="institution__jobTitle">Sr Computer Scientist, <span class="instituion__date">2010</span></h4>
+        <p class="institution__jobDescription">Sodales neque sodales ut etiam sit amet. Fringilla ut morbi tincidunt augue interdum velit. Interdum velit euismod in pellentesque.</p>
+        <h4 class="institution__jobTitle">Sr Experience Designer, <span class="instituion__date">2010</span></h4>
+        <p class="institution__jobDescription">Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec. Consequat ac felis donec et odio pellentesque diam volutpat. Hac habitasse platea dictumst quisque sagittis purus sit.</p>
+      </section>
+      <section class="institution company">
+        <div class="hr--2"></div>
+        <h3 class="institution__name">Rain</h3>
+        <h4 class="institution__jobTitle">Director of User Experience, <span class="instituion__date">2010</span></h4>
+        <p class="institution__jobDescription">Vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras. Urna nunc id cursus metus aliquam eleifend. Ipsum dolor sit amet consectetur adipiscing. Viverra tellus in hac habitasse platea. Vel eros donec ac odio. Tortor aliquam nulla facilisi cras fermentum odio.</p>
+      </section>
+    </section>
+    <section class="section section--education">
+      <h2 class="section__title">Education</h2><hr>
+      <section class="institution university">
+        <h3 class="institution__name">Harvard University</h3>
+        <div class="institution__degree">Digital Media Design (ALM)</div>
+        <div class="institution__cert">Web Technologies Graduate Certificate</div>
+      </section>
+      <section class="institution university">
+        <h3 class="institution__name">Brigham Young University</h3>
+        <div class="university__degree">Physics Teaching (BS)</div>
+        <div class="university__minor">Mathematics Minor</div>
+      </section>
+    </section>
+    <section class="section section--teachingHistory">
+      <h2 class="section__title">Teaching</h2><hr>
+      <section class="institution university">
+        <h3 class="institution__name">Brigham Young University - Idaho</h3>
+        <div class="university__className">Interaction Design ART 337R</div>
+      </section>
+      <section class="institution university">
+        <h3 class="institution__name">University of Nevada, Las Vegas</h3>
+        <div class="university__className">HTML/CSS I &amp; II</div>
+      </section>
+      <section class="institution university">
+        <h3 class="institution__name">Brigham Young University</h3>
+        <div class="university__className">Advanced Web Development</div>
+      </section>
+    </section>
+    <section class="section section--publications">
+      <h2 class="section__title">Publications</h2><hr>
+      <section class="institution publisher">
+        <h3 class="institution__name">Egghead.io</h3>
+        <div class="publisher__className">CSS Selectors in Depth</div>
+        <div class="publisher__className">Style an Application from Start to Finish</div>
+        <div class="publisher__className">Flexbox Fundamentals</div>
+      </section>
+      <section class="institution publisher">
+        <h3 class="institution__name">O'Reilly</h3>
+        <div class="publisher__className">Flex 4 Cookbook</div>
+      </section>
+    </section>
+    <section class="section section--osProjects">
+      <h2 class="section__title">Open Source Projects</h2><hr>
+      <section class="osProject">
+        <h3 class="osProject__name">Spectrum CSS</h3>
+        <div class="osProject__role">Founder/Lead Engineer</div>
+      </section>
+      <section class="osProject">
+        <h3 class="osProject__name">PhoneGap</h3>
+        <div class="osProject__role">Engineer</div>
+      </section>
+      <section class="osProject">
+        <h3 class="osProject__name">Topcoat</h3>
+        <div class="osProject__role">Engineer</div>
+      </section>
+      <section class="osProject">
+        <h3 class="osProject__name">Brackets</h3>
+        <div class="osProject__role">Lead Designer</div>
+      </section>
+      <section class="osProject">
+        <h3 class="osProject__name">Adobe Flex</h3>
+        <div class="osProject__role">Lead Designer</div>
+      </section>
+    </section>
+    <section class="section section--speaking">
+      <h2>Speaking Engagements</h2><hr>
+      <section class="conference">
+        <h3 class="conference__name">Front Workshops</h3>
+        <div class="conference__sessionTitle">Design Systems at Scale</div>
+      </section>
+      <section class="conference">
+        <h3 class="conference__name">Clarity Conference</h3>
+        <div class="conference__sessionTitle">Design Systems at Scale</div>
+      </section>
+      <section class="conference">
+        <h3 class="conference__name">Front</h3>
+        <div class="conference__sessionTitle">Sometimes You Suck</div>
+      </section>
+      <section class="conference">
+        <h3 class="conference__name">All Things Open</h3>
+        <div class="conference__sessionTitle">Open Source Needs Design</div>
+      </section>
+      <section class="conference">
+        <h3 class="conference__name">Web Unleashed</h3>
+        <div class="conference__sessionTitle">Rapid HTML Prototyping</div>
+      </section>
+      <section class="conference">
+        <h3 class="conference__name">Designers Can Open Source</h3>
+        <div class="conference__sessionTitle">Frontend Design Conf</div>
+      </section>
+      <section class="conference">
+        <h3 class="conference__name">Open Source Design</h3>
+        <div class="conference__sessionTitle">Blend Conf</div>
+      </section>
+    </section>
+  </article>
+</template>
+
+<style lang="scss">
+.content {
+  max-width: 800px;
+  margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-column-gap: calc(2 * var(--body-line-height));
+  grid-row-gap: var(--body-line-height);
+  grid-row-gap: 0px;
+  grid-template-areas:
+    "header header"
+    "profile profile"
+    "workHistory workHistory"
+    "teachingHistory education"
+    "publications speaking"
+    "osProjects osProjects";
+}
+.headerMain {
+  grid-area: header;
+}
+.section--profile {
+  grid-area: profile;
+}
+.section--workHistory {
+  grid-area: workHistory;
+  .institution {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-column-gap: calc(2 * var(--body-line-height));
+    padding-bottom: calc(2 * var(--body-line-height));
+  }
+  .institution__name {
+    grid-column: 1 / span 1;
+  }
+  .institution__jobTitle {
+    grid-column: 2 / span 3;
+    // padding-left: var(--body-line-height);
+  }
+  .institution__jobDescription {
+    grid-column: 2 / span 3;
+  }
+  .hr--2 {
+    grid-column: 2 / span 3;
+  }
+}
+.instituion__date {
+  font-weight: 300;
+}
+.section--education {
+  grid-area: education;
+}
+.section--teachingHistory {
+  grid-area: teachingHistory;
+}
+.section--osProjects {
+  grid-area: osProjects;
+}
+.section--speaking {
+  grid-area: speaking;
+}
+.section--publications {
+  grid-area: publications;
+}
+</style>
